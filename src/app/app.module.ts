1a9aeea0df3d97e0b4ef40aff79ffd1f625e399e
Alexander Tewes <alexandertewes7@gmail.com>
2023-06-04 13:51:22 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShipConfigurationComponent } from './ship-configuration/ship-configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShipConfigurationComponent
   ],
   imports: [
     BrowserModule
